9bb49dca1a412eee5076495b59471f1e04d8078c
TharinduMD <mdrangika911@gmail.com>
2021-02-27 17:40:18 +0530

    Add Authentication and Box view

--- PHP/login.php
+++ PHP/login.php
@@ -1,18 +1,29 @@
 <?php
-
+session_start();
 require_once("connection.php");
 
-if (isset($_POST["login"])) {
-    $username = $_POST["username"];
+if (isset($_POST["login"], $_POST["regiNo"], $_POST["password"])) {
+    $regiNo = $_POST["regiNo"];
     $password = $_POST["password"];
-
-    $sql = "select count(Username) from user where username = '{$username}' and password = '{$password}'";
-
-    $result = mysqli_query($conn, $sql);
-    echo mysqli_num_rows($result);
-    if (mysqli_num_rows($result) == 1) {
-        echo "seccess";
+	if (isset($_POST['remember'])) {
+        $remember = $_POST["remember"];
     } else {
-        echo "fail";
+        $remember = false;
     }
+}
+
+$sql = "select * from user where Regi_Number = '$regiNo' and Password = '$password'";
+
+$result = mysqli_query($conn, $sql);
+echo mysqli_num_rows($result) . "\n";
+if (mysqli_num_rows($result) == 1) {
+    if (isset($remember)) {
+        $_SESSION['name'] = $regiNo;
+    } else {
+        session_unset();
+    }
+    echo "seccess";
+    echo $_SESSION['name'];
+} else {
+    echo "fail";
 }
